00a98bba748a04580bdfadd43be74b75e39c6ad6
Nelson Araujo <pro.nelsonaraujo@gmail.com>
2021-03-15 16:28:12 +0100

    add click effect main-menu

--- assets/js/src/header.js
+++ assets/js/src/header.js
@@ -2,12 +2,15 @@ const burger = document.getElementById('burger');
 const firstLine = document.querySelector('.first-line');
 const secondLine = document.querySelector('.second-line');
 const thirdLine = document.querySelector('.third-line');
+const mainMenu = document.getElementById('main-menu');
 
-console.log(firstLine);
+
 
 burger.addEventListener('click' , () => {
     firstLine.classList.toggle('change-first-line');
     secondLine.classList.toggle('change-second-line');
     thirdLine.classList.toggle('change-third-line');
+    mainMenu.classList.toggle('main-menu-show');
 
 })
+
