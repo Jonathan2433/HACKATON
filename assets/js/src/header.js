9c3149d3baa810bde32579370e291b698333876f
Nelson Araujo <pro.nelsonaraujo@gmail.com>
2021-03-16 11:20:27 +0100

    add media queries navbar

--- assets/js/src/header.js
+++ assets/js/src/header.js
@@ -3,14 +3,21 @@ const firstLine = document.querySelector('.first-line');
 const secondLine = document.querySelector('.second-line');
 const thirdLine = document.querySelector('.third-line');
 const mainMenu = document.getElementById('main-menu');
+const secondMenu = document.getElementById('secondary-menu')
+const portfolio = document.getElementById('portfolio')
 
 
 
-burger.addEventListener('click' , () => {
+burger.addEventListener('click', () => {
     firstLine.classList.toggle('change-first-line');
     secondLine.classList.toggle('change-second-line');
     thirdLine.classList.toggle('change-third-line');
     mainMenu.classList.toggle('main-menu-show');
 
 })
+
+
+// portfolio.addEventListener('mouseover' , () => {
+//     secondMenu.classList.toggle('second-menu-show');
+// });
 
